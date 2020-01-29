a7dbdab2b1ac9ce266c01d796cbdca15b8253be2
JensenJ <jensenbradley@icloud.com>
2020-01-29 22:56:52 +0000

    ECS Testing
    Created a testing file for using the Entity Component System within Unity. This system will be used for the project because of the performance increase. Also added hybrid rendering package.

---
+++ Assets/Scripts/EntityTesting.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Collections;
+using Unity.Mathematics;
+using Unity.Rendering;
+
+public class EntityTesting : MonoBehaviour
+{
+    [SerializeField] private Mesh mesh;
+    [SerializeField] private Material material;
+
+    private void Start()
+    {
+        EntityManager entityManager = World.Active.EntityManager;
+        EntityArchetype entityArchetype = entityManager.CreateArchetype(
+            typeof(Translation),
+            typeof(RenderMesh),
+            typeof(LocalToWorld)
+        );
+
+        NativeArray<Entity> entityArray = new NativeArray<Entity>(100000, Allocator.Temp);
+        entityManager.CreateEntity(entityArchetype, entityArray);
+
+        for (int i = 0; i < entityArray.Length; i++)
+        {
+            Entity entity = entityArray[i];
+
+            //Set position
+            entityManager.SetComponentData(entity, new Translation { 
+                Value = new float3(
+                    UnityEngine.Random.Range(10, 200),
+                    UnityEngine.Random.Range(10, 200),
+                    UnityEngine.Random.Range(10, 200)
+                ) 
+            });
+
+            entityManager.SetSharedComponentData(entity, new RenderMesh
+            {
+                mesh = mesh,
+                material = material,
+                castShadows = UnityEngine.Rendering.ShadowCastingMode.On
+            });
+        }
+
+        entityArray.Dispose();
+    }
+}
