f127ce57bd0e2fed5735e582f0a4d9c94ff85089
JensenJ <jensenbradley@icloud.com>
2020-01-30 22:34:32 +0000

    Job System Testing
    Testing the Job System with the burst compiler.

--- Assets/Scripts/EntityTesting.cs
+++ Assets/Scripts/EntityTesting.cs
@@ -6,11 +6,14 @@ using Unity.Transforms;
 using Unity.Collections;
 using Unity.Mathematics;
 using Unity.Rendering;
+using Unity.Jobs;
+using Unity.Burst;
 
 public class EntityTesting : MonoBehaviour
 {
     [SerializeField] private Mesh mesh;
     [SerializeField] private Material material;
+    [SerializeField] private bool useJobs;
 
     private void Start()
     {
@@ -21,7 +24,7 @@ public class EntityTesting : MonoBehaviour
             typeof(LocalToWorld)
         );
 
-        NativeArray<Entity> entityArray = new NativeArray<Entity>(100000, Allocator.Temp);
+        NativeArray<Entity> entityArray = new NativeArray<Entity>(1, Allocator.Temp);
         entityManager.CreateEntity(entityArchetype, entityArray);
 
         for (int i = 0; i < entityArray.Length; i++)
@@ -31,9 +34,9 @@ public class EntityTesting : MonoBehaviour
             //Set position
             entityManager.SetComponentData(entity, new Translation { 
                 Value = new float3(
-                    UnityEngine.Random.Range(10, 200),
-                    UnityEngine.Random.Range(10, 200),
-                    UnityEngine.Random.Range(10, 200)
+                    UnityEngine.Random.Range(10, 20),
+                    UnityEngine.Random.Range(10, 20),
+                    UnityEngine.Random.Range(10, 20)
                 ) 
             });
 
@@ -46,5 +49,60 @@ public class EntityTesting : MonoBehaviour
         }
 
         entityArray.Dispose();
+    }
+
+
+    //Job system testing
+    void Update()
+    {
+        float startTime = Time.realtimeSinceStartup;
+        if (useJobs)
+        {
+            NativeList<JobHandle> jobHandleList = new NativeList<JobHandle>(Allocator.Temp);
+            for (int i = 0; i < 10; i++)
+            {
+                JobHandle handle = TestTask();
+                jobHandleList.Add(handle);
+            }
+            JobHandle.CompleteAll(jobHandleList);
+            jobHandleList.Dispose();
+        }
+        else
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                TestTaskNormal();
+            }
+        }
+        Debug.Log(((Time.realtimeSinceStartup - startTime) * 1000f) + "ms");
+    }
+
+    private JobHandle TestTask()
+    {
+        TestTaskJob job = new TestTaskJob();
+        return job.Schedule();
+    }
+
+    private void TestTaskNormal()
+    {
+        float value = 0f;
+        for (int i = 0; i < 50000; i++)
+        {
+            value = math.exp10(math.sqrt(value));
+        }
+    }
+}
+
+//TestTaskJob, represents tough calculation
+[BurstCompile]
+public struct TestTaskJob : IJob 
+{
+    public void Execute()
+    {
+        float value = 0f;
+        for (int i = 0; i < 50000; i++)
+        {
+            value = math.exp10(math.sqrt(value));
+        }
     }
 }
