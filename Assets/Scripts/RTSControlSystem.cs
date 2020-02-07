15f60f0ef11c75ac46b6ea798c9a9120232fd331
JensenJ <jensenbradley@icloud.com>
2020-02-07 23:08:54 +0000

    Prototype Build System
    Very basic prototype building system by left clicking at location, which instantiates an object. The class that contains this is the RTS control system and this will eventually control key elements of the RTS, such as movement orders.

---
+++ Assets/Scripts/RTSControlSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTSControlSystem : MonoBehaviour
+{
+
+    [SerializeField] GameObject prefabBuilding1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit = Utils.GetMousePositionClickData(Camera.main);
+            Instantiate(prefabBuilding1, hit.point, Quaternion.identity);
+        }
+        
+    }
+}
