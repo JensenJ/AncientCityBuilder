152a79aa05c0e3dec450d0d73e4346c04fd5fcbe
JensenJ <jensenbradley@icloud.com>
2020-02-14 18:44:59 +0000

    Fixed Scaffolding Positioning
    Fixed Scaffolding Position so that it fits around the buildings correctly, whilst the building is being constructed.

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -3,54 +3,57 @@
 //Script to manage all building data such as what units this building produces and also attributes associated with the building.
 public class Building : MonoBehaviour
 {
-    //Building variables
-    [SerializeField] private float buildProgress = 0.0f;
-    [SerializeField] private bool isBuilding = false;
+    //Building settings
     [SerializeField] private float buildTime = 10.0f;
-    [SerializeField] private Vector3 buildingEdges;
-    [SerializeField] private int numberOfBuilders = 0;
     [SerializeField] private float buildingHealth = 0.0f;
     [SerializeField] private float maxBuildingHealth = 100.0f;
-
     [SerializeField] private GameObject scaffoldingPrefab = null;
-    [SerializeField] private GameObject[] scaffolding = new GameObject[4];
-    [SerializeField] private Vector3[] scaffoldingPosition = new Vector3[4];
+    [SerializeField] private float scaffoldingSpace = 0.0f;
+
+    //Building variables
+    private float buildProgress = 0.0f;
+    private bool isBuilding = false;
+    private Vector3 buildingEdges;
+    private int numberOfBuilders = 0;
+    private GameObject[] scaffolding = new GameObject[4];
+    private Vector3[] scaffoldingPosition = new Vector3[4];
 
     // Start is called before the first frame update
     void Start()
     {
         //Get building bounds
-        Bounds buildingBounds = GetMaxBounds(gameObject);
+        Bounds buildingBounds = GetMaxBounds(transform.GetChild(0).gameObject);
         buildingEdges = new Vector3(buildingBounds.size.x, buildingBounds.size.y, buildingBounds.size.z);
     }
 
     public void CreateScaffolding()
     {
         //Get building bounds
-        Bounds buildingBounds = GetMaxBounds(gameObject);
+        Bounds buildingBounds = GetMaxBounds(transform.GetChild(0).gameObject);
         buildingEdges = new Vector3(buildingBounds.size.x, buildingBounds.size.y, buildingBounds.size.z);
 
         //Four sides of building scaffolding
-        scaffoldingPosition[0] = new Vector3(buildingEdges.x, buildingEdges.y / 2, 0);
-        scaffoldingPosition[1] = new Vector3(0, buildingEdges.y / 2, buildingEdges.z);
-        scaffoldingPosition[2] = new Vector3(-buildingEdges.x, buildingEdges.y / 2, 0);
-        scaffoldingPosition[3] = new Vector3(0, buildingEdges.y / 2, -buildingEdges.z);
+        scaffoldingPosition[0] = new Vector3(buildingEdges.x / 2 + scaffoldingSpace, buildingEdges.y / 2, 0);
+        scaffoldingPosition[1] = new Vector3(0, buildingEdges.y / 2, buildingEdges.z / 2 + scaffoldingSpace);
+        scaffoldingPosition[2] = new Vector3(-buildingEdges.x / 2 - scaffoldingSpace, buildingEdges.y / 2, 0);
+        scaffoldingPosition[3] = new Vector3(0, buildingEdges.y / 2, -buildingEdges.z / 2 - scaffoldingSpace);
 
         //For every side of building
         for (int i = 0; i < 4; i++)
         {
-            scaffolding[i] = Instantiate(scaffoldingPrefab, transform.position + scaffoldingPosition[i], Quaternion.identity);
+            scaffolding[i] = Instantiate(scaffoldingPrefab, transform.localPosition + scaffoldingPosition[i], Quaternion.identity, transform);
             
+            //If on odd iteration
             if(i % 2 != 0)
             {
-                //scaffolding[i].transform.Rotate(0, 90, 0);
-                scaffolding[i].transform.localScale = new Vector3(buildingEdges.x, buildingEdges.y, 1.0f);
+                //Scale to fit x side
+                scaffolding[i].transform.localScale = new Vector3(buildingEdges.x + scaffoldingSpace, buildingEdges.y, 1.0f);
             }
             else
             {
-                scaffolding[i].transform.localScale = new Vector3(1.0f, buildingEdges.y, buildingEdges.z);
+                //Scale to fit z side
+                scaffolding[i].transform.localScale = new Vector3(1.0f, buildingEdges.y, buildingEdges.z + scaffoldingSpace);
             }
-            //If on odd iteration
         }
     }
 
@@ -72,7 +75,7 @@ public class Building : MonoBehaviour
             buildProgress += (Time.deltaTime / buildTime) * numberOfBuilders;
 
             //Building trnasformation (rise out of ground when building)
-            Vector3 buildingPosition = transform.position;
+            Vector3 buildingPosition = transform.GetChild(0).position;
             buildingPosition.y = 0.0f - ((1 - buildProgress) * buildingEdges.y);
 
             //Setting building health
@@ -92,7 +95,7 @@ public class Building : MonoBehaviour
                 }
             }
             //Update position
-            transform.position = buildingPosition;
+            transform.GetChild(0).position = buildingPosition;
         }
     }
 
