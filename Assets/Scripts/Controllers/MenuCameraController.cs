74fad57d8a16ea2fa5a3628f02aac4b8ccda4be0
JensenJ <jensenbradley@icloud.com>
2020-02-20 17:04:45 +0000

    Created Main Menu Scene
    Created a scene for the main menu. Also created a camera script that moves around the main menu scene to create a better looking background for when UI is added.

---
+++ Assets/Scripts/Controllers/MenuCameraController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class MenuCameraController : MonoBehaviour
+{
+    [SerializeField] private Vector2 startPosition = new Vector2();
+    [SerializeField] private Vector2 endPosition = new Vector2();
+    [SerializeField] private Vector2 moveAmount = new Vector2();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //Set start position
+        Vector3 position = transform.position;
+        position.x = startPosition.x;
+        position.z = startPosition.y;
+        transform.position = position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move camera by move amount
+        Vector3 position = transform.position;
+        position.x += Time.deltaTime * moveAmount.x;
+        position.z += Time.deltaTime * moveAmount.y;
+        transform.position = position;
+
+        //If reached destination
+        if (transform.position.x > endPosition.x || transform.position.z > endPosition.y || Vector3.Distance(transform.position, endPosition) < 1.0f)
+        {
+            //Set start position
+            position = transform.position;
+            position.x = startPosition.x;
+            position.z = startPosition.y;
+            transform.position = position;
+        }
+    }
+}
