0c076d836bc44d8dcb2ac35cb53fed37884ec9d6
JensenJ <jensenbradley@icloud.com>
2020-02-21 16:57:50 +0000

    Main Menu Tab System

---
+++ Assets/Scripts/UI/TabButton.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+//Requires an image
+[RequireComponent(typeof(Image))]
+//Implements mouse cursor events
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+    //Tabgroup and image
+    public TabGroup tabGroup;
+    public Image background;
+
+    //Callback events
+    public UnityEvent onTabSelected;
+    public UnityEvent onTabDeselected;
+
+    //When tab selected
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //Manage on tab group
+        tabGroup.OnTabSelected(this);
+    }
+
+    //When tab hovered over
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //Manage on tab group
+        tabGroup.OnTabEnter(this);
+    }
+
+    //When tab not hovered over
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //Manage on tab group
+        tabGroup.OnTabExit(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Button setup
+        background = GetComponent<Image>();
+        tabGroup.Subscribe(this);
+    }
+    //Selection callback
+    public void Select()
+    {
+        if (onTabSelected != null)
+        {
+            //Invoke callback
+            onTabSelected.Invoke();
+        }
+    }
+
+    //Deselection callback
+
+    public void Deselect()
+    {
+        if(onTabDeselected != null)
+        {
+            //Invoke callback
+            onTabDeselected.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/TabGroup.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabGroup : MonoBehaviour
+{
+    //Settings
+    [SerializeField] List<TabButton> tabButtons = null;
+    [SerializeField] Color tabIdle = new Color();
+    [SerializeField] Color tabHover = new Color();
+    [SerializeField] Color tabActive = new Color();
+    [SerializeField] TabButton selectedTab = null;
+    [SerializeField] List<GameObject> objectsToSwap = null;
+
+    //Function called on all tabbuttons to add them to this grouping
+    public void Subscribe(TabButton button)
+    {
+        if(tabButtons == null)
+        {
+            tabButtons = new List<TabButton>();
+        }
+        tabButtons.Add(button);
+    }
+
+    //When the tab is hovered over
+    public void OnTabEnter(TabButton button)
+    {
+        //Reset tabs
+        ResetTabs();
+        //Change to hover colour
+        if (selectedTab == null || button != selectedTab)
+        {
+            button.background.color = tabHover;
+        }
+    }
+
+    //When the tab is no longer hovered over
+    public void OnTabExit(TabButton button)
+    {
+        //Reset tabs
+        ResetTabs();
+    }
+
+    //When the tab is selected
+    public void OnTabSelected(TabButton button)
+    {
+        //Callback on deselect
+        if(selectedTab != null)
+        {
+            selectedTab.Deselect();
+        }
+
+        //Assign selected tab
+        selectedTab = button;
+
+        //Select callback
+        selectedTab.Select();
+
+        //Reset tabs
+        ResetTabs();
+        //Set to active colour
+        button.background.color = tabActive;
+        //Get page index for that tab
+        int index = button.transform.GetSiblingIndex();
+        //Set page index active
+        for (int i = 0; i < objectsToSwap.Count; i++)
+        {
+            if (i == index)
+            {
+                objectsToSwap[i].SetActive(true);
+            }
+            else
+            {
+                objectsToSwap[i].SetActive(false);
+            }
+        }
+    }
+
+    //Function to reset tabs
+    public void ResetTabs()
+    {
+        //For every tab button
+        foreach(TabButton button in tabButtons)
+        {
+            //If this button is selected, skip it
+            if(selectedTab != null && button == selectedTab) { continue; }
+            //Set to idle colour
+            button.background.color = tabIdle;
+        }
+    }
+}
