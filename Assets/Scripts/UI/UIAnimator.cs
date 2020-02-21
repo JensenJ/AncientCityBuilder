08f733a51388e2e89601a481326e4105864535c6
JensenJ <jensenbradley@icloud.com>
2020-02-21 23:37:16 +0000

    Created Basic UI Animator
    Created Basic UI Animator that is capable of fading and moving UI elements off screen. It is also easily expandable to create new animation types, such as scaling. Created a fade example test button in the main menu scene to demo this.

---
+++ Assets/Scripts/UI/UIAnimator.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIAnimator : MonoBehaviour
+{
+    public enum UIAnimationTypes
+    {
+        Move, 
+        Fade
+    }
+
+    public GameObject objectToAnimate;
+
+    public UIAnimationTypes animationType;
+    public LeanTweenType easeType;
+    public float duration;
+    public float delay;
+
+    public bool loop;
+    public bool pingpong;
+
+    public bool startPositionOffset;
+    public Vector3 from;
+    public Vector3 to;
+
+    private LTDescr tweenObject;
+
+    public bool showOnEnable;
+    public bool WorkOnDisable;
+
+    public void OnEnable()
+    {
+        if (showOnEnable)
+        {
+            Show();
+        }
+    }
+
+    public void Show()
+    {
+        HandleTween();
+    }
+
+    public void HandleTween()
+    {
+        if(objectToAnimate == null)
+        {
+            objectToAnimate = gameObject;
+        }
+
+        switch (animationType) 
+        {
+            case UIAnimationTypes.Fade:
+                Fade();
+                break;
+            case UIAnimationTypes.Move:
+                MoveAbsolute();
+                break;
+        }
+
+        tweenObject.setDelay(delay);
+        tweenObject.setEase(easeType);
+
+        if (loop)
+        {
+            tweenObject.loopCount = int.MaxValue;
+        }
+
+        if (pingpong)
+        {
+            tweenObject.setLoopPingPong();
+        }
+    }
+
+    public void Fade()
+    {
+        if(gameObject.GetComponent<CanvasGroup>() == null)
+        {
+            gameObject.AddComponent<CanvasGroup>();
+        }
+
+        if (startPositionOffset)
+        {
+            objectToAnimate.GetComponent<CanvasGroup>().alpha = from.x;
+        }
+        tweenObject = LeanTween.alphaCanvas(objectToAnimate.GetComponent<CanvasGroup>(), to.x, duration);
+    }
+
+    public void MoveAbsolute()
+    {
+        objectToAnimate.GetComponent<RectTransform>().anchoredPosition = from;
+        tweenObject = LeanTween.move(objectToAnimate.GetComponent<RectTransform>(), to, duration);
+    }
+
+    void SwapDirection()
+    {
+        var temp = from;
+        from = to;
+        to = temp;
+    }
+
+    public void Disable()
+    {
+        SwapDirection();
+        HandleTween();
+        tweenObject.setOnComplete(() =>
+        {
+            SwapDirection();
+            gameObject.SetActive(false);
+        });
+    }
+}
