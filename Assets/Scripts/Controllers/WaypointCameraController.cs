8fb5801f26ae777582228f6b1be246e96778410b
JensenJ <jensenbradley@icloud.com>
2020-02-21 00:28:04 +0000

    Refactored Camera Waypoint System
    Refactored the Camera Waypoint System to take into account user error. It now checks all waypoints for if they are null before accessing data about them. Also added a slider for the move speed for suggested values. Also made use of this system in the main menu.

--- Assets/Scripts/Controllers/WaypointCameraController.cs
+++ Assets/Scripts/Controllers/WaypointCameraController.cs
@@ -2,7 +2,7 @@
 
 public class WaypointCameraController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 0.5f;
+    [SerializeField] [Range(0.01f, 5.0f)] private float moveSpeed = 0.5f;
     [SerializeField] private bool randomTarget = false;
     [SerializeField] private GameObject[] waypoints = null;
     private int currentWaypoint = 0;
@@ -10,34 +10,54 @@ public class WaypointCameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Set target position
-        Vector3 targetPosition = new Vector3(waypoints[currentWaypoint].transform.position.x, 10, waypoints[currentWaypoint].transform.position.y);
-
-        //Calculate direction to new position
-        Vector3 moveDir = Vector3.Normalize(targetPosition - transform.position);
-
-        //Move the camera to the next waypoint
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
-
-        //If in range of next waypoint
-        if(Vector3.Distance(transform.position, targetPosition) < 1.0f)
+        //Null pointer check
+        if (waypoints.Length > 0)
         {
-            //If randomly targeting
-            if(randomTarget == true)
+            //Check waypoint hasn't been removed or is null
+            if (waypoints[currentWaypoint] != null)
             {
-                //Randomly generate current waypoint index
-                currentWaypoint = Random.Range(0, waypoints.Length - 1);
+                //Set target position
+                Vector3 targetPosition = new Vector3(waypoints[currentWaypoint].transform.position.x, 10, waypoints[currentWaypoint].transform.position.z);
+
+                //Calculate direction to new position
+                Vector3 moveDir = Vector3.Normalize(targetPosition - transform.position);
+
+                //Move the camera to the next waypoint
+                transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+                //If in range of next waypoint
+                if (Vector3.Distance(transform.position, targetPosition) < 1.0f)
+                {
+                    //Get the next target
+                    GetNewTarget();
+                }
             }
             else
             {
-                //Increment waypoint target
-                currentWaypoint++;
-                //If reached end of waypoint list
-                if(currentWaypoint > waypoints.Length - 1)
-                {
-                    //Restart waypoint targeting
-                    currentWaypoint = 0;
-                }
+                //Try to find new target
+                GetNewTarget();
+            }
+        }
+    }
+
+    //Function to generate a new target position
+    void GetNewTarget()
+    {
+        //If randomly targeting
+        if (randomTarget == true)
+        {
+            //Randomly generate current waypoint index
+            currentWaypoint = Random.Range(0, waypoints.Length - 1);
+        }
+        else
+        {
+            //Increment waypoint target
+            currentWaypoint++;
+            //If reached end of waypoint list
+            if (currentWaypoint > waypoints.Length - 1)
+            {
+                //Restart waypoint targeting
+                currentWaypoint = 0;
             }
         }
     }
